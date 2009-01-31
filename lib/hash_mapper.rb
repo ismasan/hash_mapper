d7c95bd97493b3f58ff8d52176fb391d6ce5d998
Ismael Celis <ismaelct@gmail.com>
2009-01-31 16:42:29 +0000

    Added info to README for nested mappers and block filters

--- lib/hash_mapper.rb
+++ lib/hash_mapper.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module HashMapper
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
   
   def maps
     @maps ||= []
