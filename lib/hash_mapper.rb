dccbd0338ddbd5f7bba41a7973295398e659fd8a
Martin Mauch <mauch@crealytics.de>
2009-10-15 13:43:38 +0200

    Removed some spaces

--- lib/hash_mapper.rb
+++ lib/hash_mapper.rb
@@ -41,7 +41,7 @@ end
 
 module HashMapper
   VERSION = '0.0.8'
-  
+
   # we need this for inheritable mappers, which is annoying because it needs ActiveSupport, kinda overkill.
   #
   def self.extended(base)
@@ -50,24 +50,24 @@ module HashMapper
       class_inheritable_accessor :maps
     end
   end
-  
+
   def map(from, to, using=nil, &filter)
     self.maps << Map.new(from, to, using)
     to.filter = filter if block_given? # Useful if just one block given
   end
-  
+
   def from(path, &filter)
     path_map = PathMap.new(path)
     path_map.filter = filter if block_given? # Useful if two blocks given
     path_map
   end
-  
+
   alias :to :from
-  
+
   def using(mapper_class)
     mapper_class
   end
-  
+
   def normalize(a_hash)
     perform_hash_mapping a_hash, :normalize
   end
@@ -75,7 +75,7 @@ module HashMapper
   def denormalize(a_hash)
     perform_hash_mapping a_hash, :denormalize
   end
-  
+
   def before_normalize(&blk)
     @before_normalize = blk
   end
@@ -91,10 +91,10 @@ module HashMapper
   def after_denormalize(&blk)
     @after_denormalize = blk
   end
-  
+
   protected
 
-  
+
   def perform_hash_mapping(a_hash, meth)
     output = {}
     # Before filter
@@ -110,18 +110,18 @@ module HashMapper
     # Return
     output
   end
-  
+
   # Contains PathMaps
   # Makes them interact
   #
   class Map
-    
+
     attr_reader :path_from, :path_to, :delegated_mapper
-    
+
     def initialize(path_from, path_to, delegated_mapper = nil)
       @path_from, @path_to, @delegated_mapper = path_from, path_to, delegated_mapper
     end
-    
+
     def process_into(output, input, meth = :normalize)
       path_1, path_2 = (meth == :normalize ? [path_from, path_to] : [path_to, path_from])
       catch :no_value do
@@ -129,9 +129,9 @@ module HashMapper
         add_value_to_hash!(output, path_2, value)
       end
     end
-    
+
     protected
-    
+
     def get_value_from_input(output, input, path, meth)
       value = path.inject(input) do |h,e|
         v = h.with_indifferent_access[e] # this does it, but uses ActiveSupport
@@ -140,8 +140,8 @@ module HashMapper
       end
       delegated_mapper ? delegate_to_nested_mapper(value, meth) : value
     end
-    
-    
+
+
     def delegate_to_nested_mapper(value, meth)
       case value
       when Array
@@ -152,7 +152,7 @@ module HashMapper
         delegated_mapper.send(meth, value)
       end
     end
-    
+
     def add_value_to_hash!(hash, path, value)
       path.inject_with_index(hash) do |h,e,i|
         if !h[e].nil? # it can be FALSE
@@ -161,51 +161,51 @@ module HashMapper
           h[e] = (i == path.size-1 ? path.apply_filter(value) : {})
         end
       end
-      
+
     end
-    
+
   end
-  
+
   # contains array of path segments
   #
   class PathMap
     include Enumerable
-    
+
     attr_reader :segments
     attr_writer :filter
-    
+
     def initialize(path)
       @path = path.dup
       @segments = parse(path)
       @filter = lambda{|value| value}# default filter does nothing
     end
-    
+
     def apply_filter(value)
       @filter.call(value)
     end
-    
+
     def each(&blk)
       @segments.each(&blk)
     end
-    
+
     def first
       @segments.first
     end
-    
+
     def last
       @segments.last
     end
-    
+
     def size
       @segments.size
     end
-    
+
     private
-    
+
     def parse(path)
       path.sub(/^\//,'').split('/').map(&:to_sym)
     end
-    
+
   end
-  
+
 end
