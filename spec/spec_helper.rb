44fca495520dd1462b5e59572ae32e815be18c3f
Iain Beeston <iain.beeston@gmail.com>
2014-07-02 12:18:45 +0100

    Stopped warnings in rspec3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,12 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  config.filter_run focus: true
+
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
 end
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
